55d4257e5ae572d201e2ff58b4fb7c4ab5ef4651
Jeremiah Wise <jeremiah.d.wise@gmail.com>
2023-09-13 14:27:25 -0500

    Add project files.

---
+++ DemoLibrary.Tests/DisplayMesagesTest.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using DemoLibrary;
+
+// Arrange, Act, Assert
+// expected and the actual
+
+namespace DemoLibrary.Tests
+{
+    public class DisplayMesagesTest
+    {
+        [Fact]
+        public void GreetingShouldReturnGoodMorningMessage()
+        {
+            //Arrange
+            DisplayMessages messages = new DisplayMessages();
+            string expected = "Good morning Tim";
+
+            //Act
+            string actual = messages.Greeting("Tim", 2);
+
+            //Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void GreetingShouldReturnGoodAfternoonMessage()
+        {
+            //Arrange
+            DisplayMessages messages = new DisplayMessages();
+            string expected = "Good afternoon Tim";
+
+            //Act
+            string actual = messages.Greeting("Tim", 14);
+
+            //Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [InlineData("Tim", 0, "Good morning Tim")]
+        [InlineData("Tim", 13, "Good afternoon Tim")]
+        [InlineData("Tim", 22, "Good evening Tim")]
+        public void GreetingShouldReturnExpectedValue(
+            string firstName,
+            int hourOfTheDay,
+            string expected)
+        {
+            //Arrange
+            DisplayMessages messages = new DisplayMessages();
+
+            //Act
+            string actual = messages.Greeting(firstName, hourOfTheDay);
+
+            //Assert
+            Assert.Equal(expected, actual);
+        }
+
+    }
+}
